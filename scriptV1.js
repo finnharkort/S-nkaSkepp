af7672f150e3603194b553935b65ee1941687605
Finn Harkort-Berge <finnharkort@gmail.com>
2021-02-18 15:30:31 +0100

    Big clean up add new script.js with cleaner and more effective code add some functionality to the old script file(scriptV1.js) scriptV1.js will only be used for reference

--- scriptV1.js
+++ scriptV1.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//player
 let pCells = document.querySelector(".player").children;
 let rotateButton = document.querySelector("#rotate");
 let pCellList = Array.from(pCells);
@@ -12,9 +13,18 @@ let pBoatDir = 1;
 let pBoatMax = 6;
 let boatAmounts = new Array(3);
 
-SetupPlayer();
-AddListeners();
-document.getElementById("boatsLeft").innerHTML = pBoatMax;
+//computer
+let cCells = document.querySelector(".computer").children;
+let cCellList = Array.from(cCells);
+let cValueList = new Array(64);
+let cSelectList = new Array(64);
+let cSelectedY;
+let cSelectedX;
+let startAddX;
+let startAddY;
+let cBoatLength = 4;
+let cBoatDir = 1;
+let cBoatMax = 6;
 
 function boatMan(boatMax) {
   console.log("hejhej");
@@ -25,52 +35,72 @@ function boatMan(boatMax) {
     case 2:
       pBoatLength = 2;
       break;
+    case 0:
+      ComputerAddBoats();
+      break;
   }
 }
 
-function SetupPlayer() {
-  for (let i = 0; i < 64; i++) {
-    pCellList = pCellList.filter((i) => i.className !== "x-value");
-    pCellList = pCellList.filter((i) => i.className !== "y-value");
-  }
-
-  for (let i = 0; i < 64; i++) {
-    pValueList[i] = pCellList[i].value;
-  }
-
-  for (let i = 0; i < 64; i++) {
-    pSelectList[i] = false;
+function boatMan2(boatMax) {
+  switch (boatMax) {
+    case 4:
+      cBoatLength = 3;
+      break;
+    case 2:
+      cBoatLength = 2;
+      break;
   }
 }
 
-function AddListeners() {
-  for (let i = 0; i < 64; i++) {
-    pCellList[i].addEventListener("mouseover", (e) => {
-      e.preventDefault();
-      pSelectedY = parseInt(pValueList[i].charAt(0));
-      pSelectedX = parseInt(pValueList[i].charAt(2));
-      displayBoat(pBoatLength, pBoatDir);
-    });
-    pCellList[i].addEventListener("mouseout", (e) => {
-      e.preventDefault();
-      pCellList[i].classList.remove("selected");
-      removeBoat(pBoatLength, pBoatDir);
-    });
-    pCellList[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      pSelectedY = parseInt(pValueList[i].charAt(0));
-      pSelectedX = parseInt(pValueList[i].charAt(2));
-      addBoat(pBoatLength, pBoatDir);
-    });
-  }
-  rotateButton.addEventListener("click", (e) => {
-    if (pBoatDir === 1) {
-      pBoatDir = 2;
-    } else {
-      pBoatDir = 1;
-    }
+document.getElementById("boatsLeft").innerHTML = pBoatMax;
+
+for (let i = 0; i < 64; i++) {
+  pCellList = pCellList.filter((i) => i.className !== "x-value");
+  pCellList = pCellList.filter((i) => i.className !== "y-value");
+  cCellList = pCellList.filter((i) => i.className !== "x-value");
+  cCellList = pCellList.filter((i) => i.className !== "y-value");
+}
+
+for (let i = 0; i < 64; i++) {
+  console.log(cCellList[i].value);
+}
+
+for (let i = 0; i < 64; i++) {
+  pValueList[i] = pCellList[i].value;
+  cValueList[i] = cCellList[i].value;
+}
+
+for (let i = 0; i < 64; i++) {
+  pSelectList[i] = false;
+  cSelectList[i] = false;
+}
+for (let i = 0; i < 64; i++) {
+  pCellList[i].addEventListener("mouseover", (e) => {
+    e.preventDefault();
+    pSelectedY = parseInt(pValueList[i].charAt(0));
+    pSelectedX = parseInt(pValueList[i].charAt(2));
+    displayBoat(pBoatLength, pBoatDir);
+  });
+  pCellList[i].addEventListener("mouseout", (e) => {
+    e.preventDefault();
+    pCellList[i].classList.remove("selected");
+    removeBoat(pBoatLength, pBoatDir);
+  });
+  pCellList[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    pSelectedY = parseInt(pValueList[i].charAt(0));
+    pSelectedX = parseInt(pValueList[i].charAt(2));
+    addBoat(pBoatLength, pBoatDir);
   });
 }
+
+rotateButton.addEventListener("click", (e) => {
+  if (pBoatDir === 1) {
+    pBoatDir = 2;
+  } else {
+    pBoatDir = 1;
+  }
+});
 
 function displayBoat(length, direction) {
   if (
@@ -208,5 +238,73 @@ function CheckEdge(length, direction, selectedX, selectedY) {
       } else {
         return false;
       }
+  }
+}
+
+function ComputerAddBoats() {
+  while (cBoatMax > 0) {
+    startAddX = 1;
+    startAddY = 1;
+    boatMan2(cBoatMax);
+    if (
+      ComputerCheckOccupied(cBoatLength, cBoatDir) === true &&
+      CheckEdge(cBoatLength, cBoatDir, startAddX, startAddY)
+    )
+      cBoatMax--;
+    switch (cBoatDir) {
+      case 1:
+        for (let i = 0; i < length; i++) {
+          cCellList[
+            cValueList.indexOf(cSelectedY + i + "," + cSelectedX)
+          ].classList.add("permSelected");
+          cCellList[
+            cValueList.indexOf(cSelectedY + i + "," + cSelectedX)
+          ].classList.remove("selected");
+          cSelectList[
+            cValueList.indexOf(cSelectedY + i + "," + cSelectedX)
+          ] = true;
+        }
+        break;
+
+      case 2:
+        for (let i = 0; i < length; i++) {
+          cCellList[
+            pValueList.indexOf(cSelectedY + "," + (cSelectedX + i))
+          ].classList.add("permSelected");
+          cCellList[
+            pValueList.indexOf(cSelectedY + "," + (cSelectedX + i))
+          ].classList.remove("selected");
+          cSelectList[
+            cValueList.indexOf(cSelectedY + "," + (cSelectedX + i))
+          ] = true;
+        }
+        break;
+    }
+  }
+
+  function ComputerCheckOccupied(length, direction) {
+    switch (direction) {
+      case 1:
+        for (let i = 0; i < length; i++) {
+          if (
+            cSelectList[cValueList.indexOf(startAddY + i + "," + startAddX)] ===
+            true
+          ) {
+            return true;
+          }
+        }
+        return false;
+      case 2:
+        for (let i = 0; i < length; i++) {
+          if (
+            cSelectList[
+              cValueList.indexOf(startAddY + "," + (startAddX + i))
+            ] === true
+          ) {
+            return true;
+          }
+        }
+        return false;
+    }
   }
 }
